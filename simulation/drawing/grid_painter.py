97c56f6a9d028b40a8baea94104cf563f4778416
matthijs <matthijs.3mail@gmail.com>
2022-07-23 00:25:43 +0200

    create temp solution for grid painter

--- simulation/drawing/grid_painter.py
+++ simulation/drawing/grid_painter.py
@@ -11,7 +11,7 @@ class GridPainter:
         self.window = window
         self.grid = grid
 
-    # TODO: FIGURE OUT WHY CELLS ARE NOT GETTING PAINTED WHEN THEY ARE NOT ENTIRELY WITHIN THE CAMERA
+    # TODO: FIX THE GRID WITH THE ZOOMING CAMERA!
     def paint(self, should_paint=True):
         if should_paint:
             # GRID
@@ -20,7 +20,8 @@ class GridPainter:
                 for j in range(self.grid.height):
                     x = i * size
                     y = j * size
-                    if self.camera.is_in_view(Vector(x, y), Vector(x + size, y + size)):
-                        offset_position = self.camera.map_to_camera(Vector(x, y))
+                    pos = Vector(x, y)
+                    if self.camera.is_in_view(pos, Vector(x + size, y + size)) and self.camera.zoom == 100:
+                        offset_position = Vector.subtract_new(pos, self.camera.position) #self.camera.map_to_camera(Vector(x, y))
                         grid_rect = pygame.Rect(offset_position.x, offset_position.y, size, size)
                         pygame.draw.rect(self.window, pygame.Color(255, 255, 255), grid_rect, 1)
