ba7bbb3820f12bca13aea0fef2222ef29baf51ba
matthijs <matthijs.3mail@gmail.com>
2022-07-29 15:00:43 +0200

    minor addition vector pool and jupyter

--- simulation/util/vector_pool.py
+++ simulation/util/vector_pool.py
@@ -3,11 +3,13 @@ from vector import Vector
 
 class VectorPool:
 
-    def __init__(self, size):
+    def __init__(self, size=20):
         self.pool = [Vector() for _ in range(size)]
 
-    def acquire(self):
-        return self.pool.pop()
+    def acquire(self, x=0, y=0):
+        vector = self.pool.pop()
+        vector.set(x, y)
+        return vector
 
     def release(self, reusable):
         self.pool.append(reusable)
